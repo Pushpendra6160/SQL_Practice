501106757855d21b5d5dfd65d06ab51d04e86ab0
Pushpendra6160 <121226604+Pushpendra6160@users.noreply.github.com>
2023-03-28 01:24:47 +0530

    Update Practice_SQL_query_excercise.sql

--- Practice_SQL_query_excercise.sql
+++ Practice_SQL_query_excercise.sql
@@ -1,6 +1,6 @@
 use psingh;
 
--- Query:
+-- Query 1:
 
 --From the doctors table, fetch the details of doctors who work in the same hospital but in different speciality.
 
@@ -30,7 +30,7 @@ from doctors d1
 inner join doctors d2 
 on d1.id <> d2.id and d1.hospital = d2.hospital and d1.speciality <> d2.speciality;
 
--- Query :
+-- Query 2:
 
 --From the login_details table, fetch the users who logged in consecutively 3 or more times.
 
@@ -68,3 +68,47 @@ case when user_name = lead(user_name,1) over(order by login_id)
 end as repeated_names
 from login_details) as x
 where x.repeated_names is not null;
+
+-- Query 3:
+
+--From the weather table, fetch all the records when London had extremely cold temperature for 3 consecutive days or more.
+
+--Note: Weather is considered to be extremely cold then its temperature is less than zero.
+
+--Table Structure:
+
+create table weather(
+id int,
+city varchar (50),
+temprature int,
+day date
+);
+
+insert into weather values
+(1, 'London', -1,'2021-01-01'),
+(2, 'London', -2,'2021-01-02'),
+(3, 'London', 4,'2021-01-03'),
+(4, 'London', 1,'2021-01-04'),
+(5, 'London', -2,'2021-01-05'),
+(6, 'London', -5,'2021-01-06'),
+(7, 'London', -7,'2021-01-07'),
+(8, 'London', 5,'2021-01-08');
+
+select id, city, temprature, day 
+from(
+	select *, 
+	case when temprature < 0
+		and lead (temprature) over(order by id) < 0
+		and lead (temprature,2) over(order by id) < 0 
+		then "yes"
+		when temprature < 0
+		and lag (temprature) over(order by id) < 0
+		and lead (temprature) over(order by id) < 0 
+		then "yes"
+		when temprature < 0
+		and lag (temprature) over(order by id) < 0
+		and lag (temprature,2) over(order by id) < 0 
+		then "yes"
+	else null end as flag
+	from weather) x
+where x.flag = "yes";
